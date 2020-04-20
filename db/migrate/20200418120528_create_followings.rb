d8b4172cb7bfbed948f02179f634f7be4b0a99f6
taraspz <vuiko.tarasius@gmail.com>
2020-04-20 13:27:49 +0300

    db/migrate/20200418120528_create_followings.rb

--- db/migrate/20200418120528_create_followings.rb
+++ db/migrate/20200418120528_create_followings.rb
@@ -1,7 +1,8 @@
 class CreateFollowings < ActiveRecord::Migration[6.0]
   def change
     create_table :followings do |t|
-      
+      t.references :user, foreign_key: true
+      t.references :follower, foreign_key: { to_table: 'users' }
     end
   end
 end
