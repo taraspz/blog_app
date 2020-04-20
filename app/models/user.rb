7016a98b5b0b7e91aa76669592f6b34155336124
taraspz <vuiko.tarasius@gmail.com>
2020-04-20 13:29:27 +0300

    changed model app/models/user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :photos,  dependent: :destroy
+
+  has_many :followers, foreign_key: "user_id"
+  has_many :followers, foreign_key: "follower_id"
 end
