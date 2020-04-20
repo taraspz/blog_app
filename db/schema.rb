82b11d2009d56cb8271697e836493b4d54dd4328
taraspz <vuiko.tarasius@gmail.com>
2020-04-20 13:31:54 +0300

    db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,59 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_18_093756) do
+ActiveRecord::Schema.define(version: 2020_04_20_091314) do
 
-  create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table "comments", force: :cascade do |t|
+    t.datetime "createt_at"
+    t.string "commented_text"
+    t.integer "user_id"
+    t.integer "photo_id"
+    t.index ["photo_id"], name: "index_comments_on_photo_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
+  create_table "followings", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "follower_id"
+    t.index ["follower_id"], name: "index_followings_on_follower_id"
+    t.index ["user_id"], name: "index_followings_on_user_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "photo_id"
+    t.index ["photo_id"], name: "index_likes_on_photo_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.datetime "createt_at"
+    t.integer "likes_count"
+    t.integer "user_id"
+    t.integer "location_id"
+    t.text "image_data"
+    t.index ["location_id"], name: "index_photos_on_location_id"
+    t.index ["user_id"], name: "index_photos_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "user_name"
+    t.string "email"
+    t.string "password"
+  end
+
+  add_foreign_key "comments", "photos"
+  add_foreign_key "comments", "users"
+  add_foreign_key "followings", "users"
+  add_foreign_key "followings", "users", column: "follower_id"
+  add_foreign_key "likes", "photos"
+  add_foreign_key "likes", "users"
+  add_foreign_key "photos", "locations"
+  add_foreign_key "photos", "users"
 end
