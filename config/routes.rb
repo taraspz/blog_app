f9d5889919eaba0d1a39da1eb00b207e46e4cd74
taraspz <vuiko.tarasius@gmail.com>
2020-04-29 22:32:41 +0300

    other files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
-  resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
+
+  devise_for :users
+
+  resources :images do
+  resources :likes
+  end
+  post "follow/user" => "users#follow_user", as: :follow_user
+  get '/upload', to: 'images#new'
+  resources :images, only: [:index, :show, :create]
+
+  get '/@:username', to: 'users#show', as: :profile
 end
