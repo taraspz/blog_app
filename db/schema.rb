20da27526627ac70d7c8d1c2da8b70209f0be6c6
taraspz <vuiko.tarasius@gmail.com>
2020-04-29 22:32:00 +0300

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -2,67 +2,78 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
-# be faster and is potentially less error prone than running all of your
-# migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use external dependencies or application code.
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_20_091314) do
+ActiveRecord::Schema.define(version: 2020_04_29_165010) do
 
-  create_table "comments", force: :cascade do |t|
-    t.datetime "createt_at"
-    t.string "commented_text"
-    t.integer "user_id"
-    t.integer "photo_id"
-    t.index ["photo_id"], name: "index_comments_on_photo_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table "followings", force: :cascade do |t|
-    t.integer "user_id"
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "followers", force: :cascade do |t|
     t.integer "follower_id"
-    t.index ["follower_id"], name: "index_followings_on_follower_id"
-    t.index ["user_id"], name: "index_followings_on_user_id"
+    t.integer "following_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["follower_id"], name: "index_followers_on_follower_id"
+    t.index ["following_id"], name: "index_followers_on_following_id"
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "description"
+    t.index ["user_id"], name: "index_images_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
-    t.integer "photo_id"
-    t.index ["photo_id"], name: "index_likes_on_photo_id"
+    t.integer "image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["image_id"], name: "index_likes_on_image_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
-  create_table "locations", force: :cascade do |t|
-    t.string "name"
-  end
-
-  create_table "photos", force: :cascade do |t|
-    t.datetime "createt_at"
-    t.integer "likes_count"
-    t.integer "user_id"
-    t.integer "location_id"
-    t.text "image_data"
-    t.index ["location_id"], name: "index_photos_on_location_id"
-    t.index ["user_id"], name: "index_photos_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "username", null: false
     t.string "first_name"
     t.string "last_name"
-    t.string "user_name"
-    t.string "email"
-    t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "description"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
-  add_foreign_key "comments", "photos"
-  add_foreign_key "comments", "users"
-  add_foreign_key "followings", "users"
-  add_foreign_key "followings", "users", column: "follower_id"
-  add_foreign_key "likes", "photos"
-  add_foreign_key "likes", "users"
-  add_foreign_key "photos", "locations"
-  add_foreign_key "photos", "users"
 end
