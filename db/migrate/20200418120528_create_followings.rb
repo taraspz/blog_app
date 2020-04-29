13173297479d0551969104ee1f75eca5959e2839
taraspz <vuiko.tarasius@gmail.com>
2020-04-29 22:31:14 +0300

    modified migration

--- db/migrate/20200418120528_create_followings.rb
+++
@@ -1,8 +0,0 @@
-class CreateFollowings < ActiveRecord::Migration[6.0]
-  def change
-    create_table :followings do |t|
-      t.references :user, foreign_key: true
-      t.references :follower, foreign_key: { to_table: 'users' }
-    end
-  end
-end
